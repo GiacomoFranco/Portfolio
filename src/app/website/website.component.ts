e0eed088dedd820d5461466b3deefbac510488c7
Santy <santiago.gil@sinapsis.global>
2024-03-10 20:41:20 -0500

    ✨ Adding the footer component

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScrollComponent } from './components/scroll/scroll.component';
 import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-website',
   standalone: true,
-  imports: [RouterOutlet, ScrollComponent, AsideComponent],
+  imports: [RouterOutlet, ScrollComponent, AsideComponent, FooterComponent],
   templateUrl: './website.component.html',
   styleUrl: './website.component.scss',
 })
