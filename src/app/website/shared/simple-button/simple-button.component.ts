e814ffc6db4bd5701961ef1d2ff50893153ac322
Santy <santiago.gil@sinapsis.global>
2024-05-23 21:15:21 -0500

    🚧 Refactoring simple button component

--- src/app/website/shared/simple-button/simple-button.component.ts
+++ src/app/website/shared/simple-button/simple-button.component.ts
@@ -1,16 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-simple-button',
   standalone: true,
-  imports: [],
-  templateUrl: './simple-button.component.html',
-  styleUrl: './simple-button.component.scss'
+  imports: [RouterLink],
+  template: `
+    <a
+      class="redirection-button__simple"
+      [href]="button.redirectsTo"
+      target="_blank"
+      >{{ button.title
+      }}<img
+        class="arrow-icon"
+        src="/assets/images/go-to-section-icon-green.svg"
+        alt="Go to section icon"
+    /></a>
+  `,
+  styleUrl: './simple-button.component.scss',
 })
 export class SimpleButtonComponent {
-  @Input('buttonOptions') button:
-  {
-    title: string,
-    redirectsTo: string
+  @Input('buttonOptions') button: {
+    title: string;
+    redirectsTo: string;
   };
 }
