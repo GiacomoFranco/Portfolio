09c24067e33e4b9e7de408550080ecc2d4fbbbad
Santy <santiago.gil@sinapsis.global>
2024-05-27 00:03:07 -0500

    🚧 Adding caracteristics images

--- src/app/website/views/home/caracteristics/caracteristics.component.ts
+++ src/app/website/views/home/caracteristics/caracteristics.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-caracteristics',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './caracteristics.component.html',
   styleUrl: './caracteristics.component.scss'
 })
