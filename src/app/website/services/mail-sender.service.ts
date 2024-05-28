07ba5337a9ff4a1b0ccc30b2f2b126df42f9d5a9
Santy <santiago.gil@sinapsis.global>
2024-05-27 19:14:36 -0500

    🚧 Updating sender logic

--- src/app/website/services/mail-sender.service.ts
+++ src/app/website/services/mail-sender.service.ts
@@ -1,17 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BrevoBody, BrevoTemplateParameters } from '../interfaces/mail.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MailSenderService {
-  private readonly API_URL: string = 'https://urlApi';
+  private readonly API_URL: string = 'https://api.brevo.com/v3/smtp/email';
+  private readonly BREVO__APIKEY: string = '';
+  brevoHeaders = {
+    'api-key': this.BREVO__APIKEY,
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  };
+  params: BrevoTemplateParameters;
+  body: BrevoBody;
 
   constructor(private http: HttpClient) {}
 
-  ans: any;
+  sendMessage(message: any): any {
+    this.params = {
+      name: message.name,
+      email: message.email,
+      message: message.message,
+    };
 
-  sendQuestion(mensaje: any): any {
-    return this.http.post<any>(`${this.API_URL}send-email`, mensaje);
+    this.body = {
+      sender: { name: 'Francoder_', email: 'passiflora.freelance@gmail.com' },
+      to: [{ email: 'passiflora.freelance@gmail.com', name: 'Santiago' }],
+      subject: `🧙🏻‍♂️ ✨ | Francoder_, ${message.name} nos ha enviado un nuevo mensaje desde nuestro portafolio`,
+      templateId: 4,
+      params: this.params,
+    };
+
+    return this.http.post<any>(`${this.API_URL}`, this.body, {
+      headers: this.brevoHeaders,
+    });
   }
 }
