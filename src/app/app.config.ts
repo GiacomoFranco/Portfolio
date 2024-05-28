b8ceb9b2aafaf58d742e063ad7d78f988dc504ff
Santy <santiago.gil@sinapsis.global>
2024-05-27 20:04:30 -0500

    🔒️ Adding ReCaptcha configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +14,9 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: import.meta.env['NG_APP_RECAPTCHA_SITE_KEY'],
+    },
   ],
 };
