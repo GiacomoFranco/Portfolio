f523dbef3a7a5362fd4c2e9cc28636b0943aef3e
francoder_ <passiflora.freelance@gmail.com>
2025-09-02 18:39:20 -0500

    🐛 Fixing removed directive importation

--- src/app/website/views/home/about/about.component.ts
+++ src/app/website/views/home/about/about.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CounterUpDirective } from '@app/website/shared/directives/counter-up.directive';
-import { ScrollMaskedRevealDirective } from '@app/website/shared/directives/scroll-masked-reveal.directive';
 import { ScrollRevealDirective } from '@app/website/shared/directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [ScrollRevealDirective, CounterUpDirective, ScrollMaskedRevealDirective],
+  imports: [ScrollRevealDirective, CounterUpDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
